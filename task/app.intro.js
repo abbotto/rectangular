99b24a749cafa4ee8a404d80974f8de1681edce5
J. Abbott <abbotto@users.noreply.github.com>
2016-12-23 14:09:58 -0330

    Update app.intro.js

--- task/app.intro.js
+++ task/app.intro.js
@@ -1,4 +1,8 @@
 const sh = require("shelljs");
-sh.exec("./node_modules/asciify/bin/asciify.js 'Rectangular' -f rev");
+
+// Load environment variables
+require("dotenv").config();
+
+sh.exec("./node_modules/asciify/bin/asciify.js process.env.APP_NAME -f rev");
 console.log("Compiling app...\n\n");
 sh.exec("sleep 1");
