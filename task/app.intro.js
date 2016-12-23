3c0c1e44e6c011fc61de26bff4816597ba404ce5
J. Abbott <abbotto@users.noreply.github.com>
2016-12-23 14:11:36 -0330

    Update app.intro.js

--- task/app.intro.js
+++ task/app.intro.js
@@ -2,7 +2,9 @@ const sh = require("shelljs");
 
 // Load environment variables
 require("dotenv").config();
+
 const name = process.env.APP_NAME;
 sh.exec("./node_modules/asciify/bin/asciify.js " + name + " -f rev");
+
 console.log("Compiling app...\n\n");
 sh.exec("sleep 1");
