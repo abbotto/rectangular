8230cfecb050d2014880874ce4eb638a6d641bf9
Jared Abbott <jaredabbott@bluedrop.com>
2017-02-04 20:25:59 -0330

    Removed UI services

--- src/client/shared/service/ui/ui.colors.service.js
+++
@@ -1,9 +0,0 @@
-(() => {
-	"use strict";
-	
-	const materialColorsService = function materialColorsService($mdColors) {
-		return $mdColors;
-	};
-	
-	angular.module("ui.colors.service", []).factory("colors$", materialColorsService);
-})();
