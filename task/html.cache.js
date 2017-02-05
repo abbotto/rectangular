b5c4aa7e090d31c9e182b55945a068b31232f768
Jared Abbott <jaredabbott@bluedrop.com>
2017-02-04 22:03:31 -0330

    delint

--- task/html.cache.js
+++ task/html.cache.js
@@ -5,17 +5,19 @@ const files = finder.sync([
 	"./src/client/component/**/*.html"
 ]);
 const tmpJS = "tmp/templates.js";
-let templates = [], key, value, cache;
+let templates = [], key, value;
 
 files.forEach((path) => {
 	key = path
-	.replace("./src/client/", "")
-	.replace("component/", "")
-	.replace("shared/", "")
+		.replace("./src/client/", "")
+		.replace("component/", "")
+		.replace("shared/", "")
+	;
+	
 	value = JSON.stringify(fs.readFileSync(path, "utf8"));
 	templates.push("$templateCache.put(\"" + key + "\"," + value + ")");
 });
 
-cache = templates.join(";");
+const cache = templates.join(";");
 templates = "(function(){angular.module(\"app.template\", []).run(function($templateCache) {" + cache + "})})()";
 fs.writeFile(tmpJS, templates);
