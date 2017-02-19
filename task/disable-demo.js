f0446127f180adafa68fa73ebe178d8f196089f5
Jared Abbott <jaredabbott@bluedrop.com>
2017-02-18 20:43:22 -0330

    Removed demo files

--- task/disable-demo.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-const sh = require("shelljs");
-sh.exec("rm -rf ./src/client/component/excuse");
-sh.exec("cp ./task/disable-demo/app.route.js ./src/client/registry/app.route.js");
-sh.exec("cp ./task/disable-demo/home.component.html ./src/client/component/home/home.component.html");
--- task/enable-demo.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-const sh = require("shelljs");
-sh.exec("cp -a ./task/enable-demo/excuse/. ./src/client/component/excuse ");
-sh.exec("cp ./task/enable-demo/app.route.js ./src/client/registry/app.route.js");
-sh.exec("cp ./task/enable-demo/home.component.html ./src/client/component/home/home.component.html");
