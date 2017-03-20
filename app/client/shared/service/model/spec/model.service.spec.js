bdf4d522e69ccd695dd3a3384596fb1311a08aa9
Jared Abbott <abbotto@users.noreply.github.com>
2017-03-20 10:29:41 -0230

    Updates

--- app/client/shared/service/model/spec/model.service.spec.js
+++ app/client/shared/service/model/spec/model.service.spec.js
@@ -1,15 +1,13 @@
 "use strict";
 
 describe("model$ service", () => {
-	var appModel;
 	beforeEach(() => {
 		test.module("model.service");
 		test.inject("model$");
-		test.stub(appModel, { "project": { "name": "Rectangular" } });
+		test.mock("appModel", (key) => { const o = { "project": { "name": "Rectangular" } }; return o[key];});
 	});
 
 	it("should return the correct value", () => {
-		console.log(model$.project);
-		test.expect(model$.project.name).to.equal("Rectangular");
+		test.expect(model$("project").name).to.equal("Rectangular");
 	});
 });
