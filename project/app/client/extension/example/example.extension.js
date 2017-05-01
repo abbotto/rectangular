00e72101bafe4c39f84470c846c3cfe18f772646
Jared Abbott <abbotto@users.noreply.github.com>
2017-05-01 02:01:29 -0230

    Removed a file

--- project/app/client/extension/example/example.extension.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-(() => {
-	const exampleVendorService = function exampleVendorService(example) {
-		return example;
-	};
-	
-	angular.module("example.extension", []).factory("example$", exampleVendorService);
-})();
