99e8cab7791789aeb3709fe5924c2fd8a5f5c999
Jared Abbott <abbotto@users.noreply.github.com>
2017-05-03 09:17:40 -0230

    Edits

--- dev/test/test.inject.js
+++ dev/test/test.inject.js
@@ -3,11 +3,8 @@
 // inject lets you easily inject Angular deps using BardJS
 
 // Example
-// __.inject(
-//     'arg1',
-//     'arg2',
-//     'arg3'
-// )
+// __.inject("foo.service")
+
 (() => {
 	__.inject = function testInject() {
 		const args = Array.prototype.slice.call(arguments);
