60bf5dcb93beccebaf9ae7d5da45aa96c1177378
Jared Abbott <abbotto@users.noreply.github.com>
2017-05-03 13:56:59 -0230

    Edits

--- dev/task/script.compile.js
+++ dev/task/script.compile.js
@@ -24,7 +24,7 @@ const EOL = require("os").EOL;
 
 const sourceJSON = finder
 	.sync(
-		.concat(require("./../../dev/task/asset/source.js.json"))
+		require("./../../dev/task/asset/source.js.json")
 		.concat(require("./../../tmp/src/dev/task/asset/source.js.json"))
 	)
 ;
