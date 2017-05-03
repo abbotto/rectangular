6da455b34b8d5ada5f6046fc7f5da7d05a50520c
Jared Abbott <abbotto@users.noreply.github.com>
2017-05-03 12:49:53 -0230

    Edits

--- dev/task/style.compile.js
+++ dev/task/style.compile.js
@@ -3,16 +3,15 @@
 const sh = require("shelljs");
 const fs = require("fs");
 const finder = require("glob-concat");
-const sources = require("./../../dev/task/asset/source.scss.json").concat(require("./../../tmp/src/dev/task/asset/source.scss.json"));
+const tmpCSSPath = "tmp/source.scss";
 
-const appSCSS = finder.sync(sources);
-const tmpCSS = "tmp/source.scss";
+const appJSON = finder
+	.sync(
+		require("./../../dev/task/asset/source.scss.json")
+		.concat(require("./../../tmp/src/dev/task/asset/source.scss.json"))
+	)
+;
 
-// Commands
-const postCSS = "chmod +x node_modules/postcss/lib/postcss.js && node node_modules/postcss/lib/postcss.js";
-const nodeSASS = "chmod +x node_modules/node-sass/bin/node-sass && node_modules/node-sass/bin/node-sass";
-
-// Vendor files
 const vendorJSON = finder
 	.sync(
 		require("./../../dev/task/asset/vendor.scss.json")
@@ -20,16 +19,22 @@ const vendorJSON = finder
 	)
 ;
 
+// Commands
+const postCSS = "chmod +x node_modules/postcss/lib/postcss.js && node node_modules/postcss/lib/postcss.js";
+const nodeSASS = "chmod +x node_modules/node-sass/bin/node-sass && node_modules/node-sass/bin/node-sass";
+
+// Vendor files
+
 // Preflight
 sh.exec("node_modules/stylelint/bin/stylelint.js app/**/*.scss tmp/src/app/**/*.scss");
 sh.exec("node dev/task/font.copy.js");
 sh.exec("node dev/task/image.copy.js");
 
 // Build
-sh.cat(appSCSS).to(tmpCSS);
+sh.cat(appJSON).to(tmpCSSPath);
 
-sh.exec(nodeSASS + " -q --output-style compressed --include-path scss " + tmpCSS + " " + tmpCSS);
-sh.exec(postCSS + " --use autoprefixer -b 'last 5 versions' < " + tmpCSS);
+sh.exec(nodeSASS + " -q --output-style compressed --include-path scss " + tmpCSSPath + " " + tmpCSSPath);
+sh.exec(postCSS + " --use autoprefixer -b 'last 5 versions' < " + tmpCSSPath);
 
-vendorJSON.push(tmpCSS);
+vendorJSON.push(tmpCSSPath);
 sh.cat(vendorJSON).to("dist/app.css");
