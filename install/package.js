6aebbec779709c83c63d4bb87f5732c2177ac164
Jared Abbott <abbotto@users.noreply.github.com>
2017-05-06 14:57:03 -0230

    Fixed bugs

--- install/package.js
+++ install/package.js
@@ -6,4 +6,6 @@ const packageJSON = require("./../tmp/project/package.json");
 packageJSON.name = process.argv[0];
 packageJSON.description = process.argv[1];
 
+packageJSON = JSON.stringify(packageJSON);
+
 fs.writeFile("tmp/project/package.json", packageJSON);
