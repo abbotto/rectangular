b122f7ade1b650a93933de081a6e7c2be0742ec8
Jared Abbott <abbotto@users.noreply.github.com>
2017-05-06 14:51:17 -0230

    Refactor

--- install/package.js
+++ install/package.js
@@ -1,12 +1,12 @@
 "use strict";
 
 const fs = require("fs");
-let packageJSON = JSON.parse(require("./../tmp/project/package.json"));
+let packageJSON = require("./../tmp/project/package.json");
 
 packageJSON.name = process.argv(0);
 packageJSON.description = process.argv(1);
 
 // Update paths to point to the root project directory
-packageJSON = JSON.stringify(packageJSON);
+packageJSON = packageJSON;
 
 fs.writeFile("tmp/project/package.json", packageJSON);
