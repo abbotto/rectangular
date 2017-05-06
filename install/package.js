ff8e19374129b33b76518890937f786b541c7f37
Jared Abbott <abbotto@users.noreply.github.com>
2017-05-06 15:40:09 -0230

    Refactor

--- install/package.js
+++ install/package.js
@@ -1,12 +1,15 @@
 "use strict";
 
 const fs = require("fs");
+const sh = require("shelljs");
 const args = process.argv.slice(2);
-let packageJSON = require("./../tmp/project/package.json");
+
+sh.exec("curl -O https://raw.githubusercontent.com/abbotto/rectangular/master/install/project/package.json");
+let packageJSON = require("package.json");
 
 packageJSON.name = args[0];
 packageJSON.description = args[1];
 
 packageJSON = JSON.stringify(packageJSON);
 
-fs.writeFile("tmp/project/package.json", packageJSON);
+fs.writeFile("package.json", packageJSON);
