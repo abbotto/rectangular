24b3abee4d4eeaa944a80011cc86251a19532f5f
Jared Abbott <abbotto@users.noreply.github.com>
2017-05-06 12:25:33 -0230

    Refactor

--- dev/vendor/shared/service/ui/bootstrap/ui.tooltip.service.js
+++
@@ -1,9 +0,0 @@
-(() => {
-	"use strict";
-	
-	const bootstrapTooltipService = function bootstrapTooltipService($tooltip) {
-		return $tooltip;
-	};
-	
-	angular.module("ui.tooltip.service", []).factory("tooltip$", bootstrapTooltipService);
-})();
--- dev/vendor/shared/service/ui/material-design/ui.sticky.service.js
+++
@@ -1,9 +0,0 @@
-(() => {
-	"use strict";
-	
-	const materialStickyService = function materialStickyService($mdSticky) {
-		return $mdSticky;
-	};
-	
-	angular.module("ui.sticky.service", []).factory("sticky$", materialStickyService);
-})();
