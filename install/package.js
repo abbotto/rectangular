9cdc3b24eca4a63638c58ebad0aa8ab544869849
Jared Abbott <abbotto@users.noreply.github.com>
2017-05-06 16:39:41 -0230

    Refactor

--- install/package.js
+++ install/package.js
@@ -2,11 +2,9 @@
 
 const fs = require("fs");
 const args = process.argv.slice(2);
-let packageJSON = require(__dirname + "/package.json");
+const packageJSON = require(__dirname + "/package.json");
 
 packageJSON.name = args[0];
 packageJSON.description = args[1];
 
-packageJSON = JSON.stringify(packageJSON);
-
-fs.writeFile("package.json", packageJSON);
+fs.writeFile("package.json", JSON.stringify(packageJSON));
