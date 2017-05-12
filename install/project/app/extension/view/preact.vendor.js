7743badc2ad4031ccec4c9b254da3f3b7b93b235
Jared Abbott <abbotto@users.noreply.github.com>
2017-05-12 12:53:10 -0230

    Virtual DOM

--- install/project/app/extension/view/preact.vendor.js
+++ install/project/app/extension/view/preact.vendor.js
@@ -2,12 +2,15 @@
 
 (() => {
 	const preactVendorService = function preactVendorService($templateCache) {
+		const HTMLtoJSX = require("htmltojsx");
+		const converter = new HTMLtoJSX();
+
 		// eslint-disable-next-line no-undef
 		preact.template = (tpl) => {
 			// eslint-disable-next-line no-undef
-			return $templateCache.get(tpl));
+			return converter.convert($templateCache.get(tpl));
 		};
-		
+
 		// eslint-disable-next-line no-undef
 		return preact;
 	};
@@ -15,5 +18,5 @@
 	angular
 		.module("preact.vendor.service", [])
 		.factory("view$", preactVendorService)
-	;
+		;
 })();
