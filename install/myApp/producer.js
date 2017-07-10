af8ebef9b56e6ff1235360803f47f2670f75b7d0
Jared Abbott <abbotto@users.noreply.github.com>
2017-07-10 08:49:58 -0230

    Specs are passing

--- install/myApp/producer.js
+++ install/myApp/producer.js
@@ -1,26 +1,35 @@
+"use strict";
+
+// --------------------------------
+// Setup
+// --------------------------------
 require("dotenv").config();
 
-const args = process.argv.slice(2);
 const sh = require("shelljs");
-const NODE_ENV = process.env.NODE_ENV;
-const isProduction = NODE_ENV === "production";
 const uglifyRules = require("./uglify.json");
 const deps = require("./deps.json");
+
+const args = process.argv.slice(2);
+const NODE_ENV = process.env.NODE_ENV;
+const isProduction = NODE_ENV === "production";
 
 // --------------------------------
 // Flags
 // --------------------------------
-args.indexOf("--clean") > -1 && sh.exec("rm -rf dist && mkdir dist && rm -rf tmp && mkdir tmp")
-args.indexOf("--env") > -1 && require("./dev/bundle/task/env.js")(".envrc", __dirname)
-args.indexOf("--font") > -1 && require("./dev/bundle/task/font.js")(deps.font, __dirname)
-args.indexOf("--image") > -1 && require("./dev/bundle/task/image.js")(deps.image, __dirname)
-args.indexOf("--model") > -1 && require("./dev/bundle/task/model.js")(deps.model, __dirname)
-args.indexOf("--route") > -1 && require("./dev/bundle/task/route.js")(["./app/**/*.route.js"], __dirname)
-args.indexOf("--script") > -1 && require("./dev/bundle/task/script.js")(deps.script, __dirname)
-args.indexOf("--style") > -1 && require("./dev/bundle/task/style.js")(deps.style, __dirname)
-args.indexOf("--template") > -1 && require("./dev/bundle/task/template.js")(deps.template, __dirname)
-args.indexOf("--index") > -1 && sh.exec("cp app/index.html dist/")
+args.indexOf("--clean") > -1 && sh.exec("rm -rf dist && mkdir dist && rm -rf tmp && mkdir tmp");
+args.indexOf("--env") > -1 && require("./dev/bundle/task/env.js")(".envrc", __dirname);
+args.indexOf("--font") > -1 && require("./dev/bundle/task/font.js")(deps.font, __dirname);
+args.indexOf("--image") > -1 && require("./dev/bundle/task/image.js")(deps.image, __dirname);
+args.indexOf("--model") > -1 && require("./dev/bundle/task/model.js")(deps.model, __dirname);
+args.indexOf("--route") > -1 && require("./dev/bundle/task/route.js")(["./app/**/*.route.js"], __dirname);
+args.indexOf("--script") > -1 && require("./dev/bundle/task/script.js")(deps.script, __dirname);
+args.indexOf("--style") > -1 && require("./dev/bundle/task/style.js")(deps.style, __dirname);
+args.indexOf("--template") > -1 && require("./dev/bundle/task/template.js")(deps.template, __dirname);
+args.indexOf("--index") > -1 && sh.exec("cp app/index.html dist/");
 
+// --------------------------------
+// Bundles
+// --------------------------------
 const {
 	BabelPlugin,
 	CSSPlugin,
@@ -79,7 +88,7 @@ if (args.indexOf("--spec") > -1 || args.indexOf("-s") > -1) {
 
 if (NODE_ENV === "development") {
 	// vendor.hmr().watch();
-	// app.watch("app/**");
+	app.watch("app/**/*.js");
 }
 
 if (args.indexOf("--build") > -1) {
--- install/myApp/server.js
+++ install/myApp/server.js
@@ -1,5 +1,11 @@
+"use strict";
+
+const sh = require("shelljs");
+
 const port = 4444;
 const bundle = require("./producer.js");
+
+sh.exec("node producer.js --env --font --index --image --model --route --script --style --template --build");
 
 bundle.dev(
 	{
