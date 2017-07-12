e4373180e0a8d0be1ac57ad688245873c8c5a234
Jared Abbott <abbotto@users.noreply.github.com>
2017-07-12 17:20:01 -0230

    Edits

--- install/myApp/server.js
+++ install/myApp/server.js
@@ -29,7 +29,7 @@ module.exports = (fuse, app) => {
 		console.log("\n\"" + path + "\" has been changed...\n");
 		
 		if (path === "deps.json") sh.exec("node producer.js --env --font --image --script --model --route --template");
-		else if (path.indexOf(".html") > -1) sh.exec("node producer.js --index --template");
+		else if (path.indexOf(".html") > -1 || path.indexOf(".jsx") > -1) sh.exec("node producer.js --index --template");
 		else if (path.indexOf(".json") > -1) sh.exec("node producer.js --model");
 		else if (path.indexOf(".scss") > -1) sh.exec("node producer.js --style");
 		else if (path.indexOf(".route.js") > -1) sh.exec("node producer.js --route");
