cd5c167fb2e338f78c4c7a291a28539963afc420
Jared Abbott <abbotto@users.noreply.github.com>
2017-07-22 00:33:07 -0230

    Edits

--- install/project/dev/server.js
+++ install/project/dev/server.js
@@ -21,15 +21,17 @@ module.exports = (fuse, app) => {
 	// Hot-module reload
 	app.hmr().watch(__dirname + "/app/**");
 	
-	watch.on("change", (path, stats) => {
-		console.log("\"" + path + "\" has been changed...\n");
-		
-		if (path === "deps.json") sh.exec("node producer.js --env --font --image --script --model --route --template");
-		else if (path.indexOf(".html") > -1 || path.indexOf(".jsx") > -1) sh.exec("node producer.js --template");
-		else if (path.indexOf(".json") > -1) sh.exec("node producer.js --model");
-		else if (path.indexOf(".scss") > -1) sh.exec("node producer.js --style");
-		else if (path.indexOf(".route.js") > -1) sh.exec("node producer.js --route");
-		else if (path.indexOf(".spec.js") > -1) sh.exec("node producer.js --spec");
-		else if (path.indexOf(".js") < 0) sh.exec("node producer.js --env --font --image");
-	});
+	watch.on("change",
+		(path, stats) => {
+			console.log("\"" + path + "\" has been changed...\n");
+			
+			if (path === "deps.json") sh.exec("node producer.js --env --font --image --script --model --route --template");
+			else if (path.indexOf(".html") > -1 || path.indexOf(".jsx") > -1) sh.exec("node producer.js --template");
+			else if (path.indexOf(".json") > -1) sh.exec("node producer.js --model");
+			else if (path.indexOf(".scss") > -1) sh.exec("node producer.js --style");
+			else if (path.indexOf(".route.js") > -1) sh.exec("node producer.js --route");
+			else if (path.indexOf(".spec.js") > -1) sh.exec("node producer.js --spec");
+			else if (path.indexOf(".js") < 0) sh.exec("node producer.js --env --font --image");
+		}
+	);
 };
