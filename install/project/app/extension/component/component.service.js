ac0ac5ae5bd2fa269a0aa0c104179c1b8d230807
Jared Abbott <abbotto@users.noreply.github.com>
2017-07-23 09:26:42 -0230

    Make ReactJS optional

--- install/project/app/extension/component/component.service.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-import reactVendorService from "~/app/extension/component/react.vendor.service.js";
-
-export default angular
-	.module("component.service", [])
-	.factory("component$", reactVendorService)
-;
