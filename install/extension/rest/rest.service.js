fd0bda44555bf17571c359bd891ada4167b501e6
Jared Abbott <abbotto@users.noreply.github.com>
2018-04-29 17:23:03 -0230

    bump node version

--- install/extension/rest/rest.service.js
+++ install/extension/rest/rest.service.js
@@ -3,6 +3,6 @@
 import RestangularVendorService from "~/app/extension/rest/restangular.vendor.service.js";
 
 export default angular
-	.module("rest.service", [])
+	.module("rest.service", ['restangular'])
 	.factory("rest$", RestangularVendorService)
 ;
